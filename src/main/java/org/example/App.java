1c7ab34ceef2e6b53b73d940d1c6c66c1ae68f1a
tlovyagina <tatiana.loviagina@yandex.ru>
2023-11-08 21:55:35 +0300

    Final commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,16 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        House house = new House();
+        house.checkInTenants();
+        List<String> tenants = house.getOlder18();
+
+        if (tenants.isEmpty())
+            System.out.println("Нет жильцов старше 18 лет");
+        else
+            System.out.println("Отсортированные по алфавиту жильцы старше 18 лет: " + tenants);
     }
 }
---
+++ src/main/java/org/example/House.java
@@ -0,0 +1,31 @@
+package org.example;
+
+
+import java.util.*;
+
+public class House {
+    private Map<String, Integer> tenants = new HashMap<>();
+
+    public void checkInTenants() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите имя и возраст человека (0 -завершить список жильцов):");
+            String name = scanner.next();
+            if (name.equals("0")) break;
+            tenants.put(name, scanner.nextInt());
+        }
+    }
+
+    public List<String> getOlder18() {
+        List<String> result = new ArrayList<>();
+
+        for (String name : tenants.keySet()) {
+            if (tenants.get(name) > 18)
+                result.add(name);
+        }
+
+        Collections.sort(result);
+        return result;
+    }
+}
